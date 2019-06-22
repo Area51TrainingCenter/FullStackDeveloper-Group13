13c3042ce621381737d455491aa49f095e86676d
Alumno <alumno@A51MM-04.local>
2019-06-22 10:42:41 -0500

    Modelos con interfaces

--- Angular/Clase03/appModelosServicios/src/app/listado/listado.component.ts
+++ Angular/Clase03/appModelosServicios/src/app/listado/listado.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Novela } from '../modelos/novela';
+import { Inovela } from '../modelos/inovela';
 
 @Component({
 	selector: 'app-listado',
@@ -7,14 +8,32 @@ import { Novela } from '../modelos/novela';
 	styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent implements OnInit {
-	novelas: Array<Novela> = [
-		new Novela("El Perfume", "Historia de un asesino que mata mujeres para extraer su esencia y crear perfumes", "Patrick Suskind", 1990),
-		new Novela("Jason y los argonautas", "Narra la historia del viaje de Jason para obtener el vellocino de oro", "Herodoto", 50),
-		new Novela("El Caballero Carmelo", "Cuenta la historia de un gallo viejo que se ve forzado a luchar con el 'Aji Seco', un gallo de pelea mas joven", "Abraham Valdelomar", 1910)
+	novelas: Array<Inovela> = [
+		{
+			titulo: "El Perfume",
+			descripcion: "Historia de un asesino que mata mujeres para extraer su esencia y crear perfumes",
+			autor: "Patrick Suskind",
+			anno: 1990
+		},
+		{
+			titulo: "Jason y los argonautas",
+			descripcion: "Narra la historia del viaje de Jason para obtener el vellocino de oro", autor: "Herodoto",
+			anno: 50
+		},
+		{
+			titulo: "El Caballero Carmelo",
+			descripcion: "Cuenta la historia de un gallo viejo que se ve forzado a luchar con el 'Aji Seco', un gallo de pelea mas joven",
+			autor: "Abraham Valdelomar",
+			anno: 1910
+		}
 	]
 	constructor() { }
 
 	ngOnInit() {
+	}
+
+	intro(texto: string): string {
+		return texto.substring(0, 50)
 	}
 
 }
