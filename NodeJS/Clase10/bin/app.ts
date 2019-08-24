2fb90bafcc8b8027dbd4e8d9ba55f5d33d0de57f
Alumno <alumno@A51MM-04.local>
2019-08-24 12:55:06 -0500

    Base

--- NodeJS/Clase10/bin/app.ts
+++ NodeJS/Clase10/bin/app.ts
@@ -1,1 +1,40 @@
-console.log("Todo funciona")
+import * as http from "http"
+import * as express from "express"
+import { Application } from "express"
+
+let httpServer: http.Server
+
+const inicializar = (): Promise<any> => {
+	return new Promise((resolve, reject) => {
+		const app: Application = express()
+		httpServer = http.createServer(app)
+
+		app.get("/", (req, res) => {
+			res
+				.type("text/html")
+				.status(200)
+				.send("<h1>Hola Mundo</h1>")
+		})
+
+		/* 		httpServer.listen(3000, ()=> {
+					console.log("Servidor ejecutándose en el puerto 3000")
+					resolve()
+				}) */
+
+		httpServer.listen(3000)
+			.on("listening", () => resolve())
+			.on("error", err => reject(err))
+	})
+}
+
+const cerrar = (): Promise<any> => {
+	return new Promise((resolve, reject) => {
+		httpServer.close(err => {
+			if (err) return reject(err)
+
+			resolve()
+		})
+	})
+}
+
+export { inicializar, cerrar }
--- NodeJS/Clase10/server.ts
+++ NodeJS/Clase10/server.ts
@@ -1,1 +1,53 @@
-const nombre: string = "FullStack"
+/* import { inicializar } from "bin/app" */
+import * as app from "./bin/app"
+
+const iniciar = async () => {
+	/* 	app.inicializar()
+			.then(() => console.log("Servidor ejecutándose"))
+			.catch(error => console.log(error)) */
+
+	try {
+		console.log("Servidor inicializándose")
+		await app.inicializar()
+		console.log("Servidor ejecutándose en el puerto 3000")
+	} catch (error) {
+		console.error(error)
+		process.exit(1)
+	}
+}
+
+iniciar()
+
+const apagar = async (err = null) => {
+	let error = err
+
+	try {
+		console.log("Servidor cerrándose")
+		await app.cerrar()
+		console.log("Servidor cerrado")
+	} catch (err) {
+		console.log("Ocurrió un error al cerrar", err)
+		error = error || err
+	}
+
+	console.log("Proceso de cerrado activo")
+
+	if (error) process.exit(1)
+	else process.exit(0)
+
+}
+
+process.on("SIGTERM", () => {
+	console.log("SIGTERM recibido")
+	apagar()
+})
+
+process.on("SIGINT", () => {
+	console.log("SIGINT recibido")
+	apagar()
+})
+
+process.on("uncaughtException", err => {
+	console.log("uncaught exception")
+	apagar(err)
+})
